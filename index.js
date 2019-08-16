8c33cd6f6f1cf8ed526acad9d8d0e9db56c2de49
Vehe <vehedev@gmail.com>
2019-08-16 13:39:26 +0200

    refactor alerts

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
+const Alerts = require('./src/alerts');
 const Discord = require('discord.js');
-const request = require('request');
-const cron = require('node-cron');
 const bot = new Discord.Client();
 const fs = require('fs');
 
@@ -13,14 +12,9 @@ bot.mongostatus = true;
 bot.db = null;
 
 // Mongo server
-//const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@${process.env.MONGO_URL}/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@${process.env.MONGO_URL}/?retryWrites=true&w=majority`;
 
-// DEBUG DB
-const uri = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PW}@192.168.1.112/`;
-
-/**
- * Busca en la carpeta commands todos los archivos JS y los almacena en una collection.
- */
+// Busca en la carpeta commands todos los archivos JS y los almacena en una collection.
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
@@ -31,9 +25,9 @@ for (const file of commandFiles) {
  * Se ejecuta cuando se conecta el bot al servidor.
  */
 bot.once('ready', () => {
-    
+
     bot.guilds.forEach(guild => {
-        if(!guild.channels.exists('name','path-of-exile-bot')) {
+        if(!guild.channels.some(x => x.name === 'path-of-exile-bot')) {
             guild.createChannel('path-of-exile-bot', { type: 'text' });
         }
     });
@@ -46,7 +40,7 @@ bot.once('ready', () => {
         .setFooter('Cuando acabes de configurarme podrás comenzar.');
 
     setTimeout(function() {
-        bot.channels.findAll('name','path-of-exile-bot').forEach(channel => {
+        bot.channels.filter(w => w.name === 'path-of-exile-bot').forEach(channel => {
             channel.send(configureMessage)
         });
     }, 2000);
@@ -54,7 +48,7 @@ bot.once('ready', () => {
     connectToDB();
 
     setTimeout(function() {
-        setupAlerts();
+        Alerts.setInitialAlerts(bot);
     }, 2000);
 
 });
@@ -86,69 +80,6 @@ bot.on('message', async message => {
 });
 
 /**
- * Inicializa las alertas de los items de la base de datos.
- * ! IMPROVE
- */
-function setupAlerts()
-{
-    if(!bot.mongostatus) return;
-
-    const guildAlerts = bot.db.collection('alerts');
-
-    bot.guilds.forEach(x =>
-    {
-        const guildChannel = x.channels.find('name','path-of-exile-bot');
-        
-        guildAlerts.find({guildId:x.id}).toArray(function(err, res)
-        {
-            if(err) return;
-            var tmp = [];
-
-            res.forEach(i =>
-            {
-                var alertCron = cron.schedule(`*/${i.refresh} * * * *`, () => {
-
-                    request({url:`https://poe.ninja/api/data/${(i.class == 'Currency' || i.class == 'Fragments') ? 'currencyoverview':'itemoverview'}?league=Legion&type=${i.class.replace(/\s+/g,'')}`, json:true}, function (error, response, body) {
-                        if(error) return;
-                        body['lines'].forEach(n =>
-                        {
-                            if(n['detailsId'] == i.itemName)
-                            {
-                                var currencyID = n['pay']['pay_currency_id'];
-                                var itemImage = body['currencyDetails'][currencyID-1]['icon'].slice(0,-16);
-
-                                var tmpMsg = {
-                                    color: 0xeac100,
-                                    thumbnail: {
-                                        url: itemImage,
-                                    },
-                                    fields: [
-                                        {
-                                            name: `${x.emojis.find(emoji => emoji.name === i.class.replace(/\s+/g, ''))} ${i.itemShowName}`,
-                                            value: '\u200b'
-                                        }
-                                    ],
-                                    timestamp: new Date(),
-                                    footer: {
-                                        text: `Esta alerta se volvera a ejecutar dentro de ${i.refresh} minutos.`
-                                    }
-                                }
-                                guildChannel.send({embed:tmpMsg});
-                            }
-                        });
-                    });
-                });
-
-                alertCron.alert = i.itemName;
-                tmp.push(alertCron);
-            });
-
-            bot.alerts.set(x.id, tmp);
-        });
-    });
-}
-
-/**
  * Iniciamos la conexión con la base de datos.
  * ! FIX: No reconecta cuanodo al iniciar el bot la base de datos esta caida.
  */
@@ -168,15 +99,15 @@ function connectToDB()
         .addField('Se ha restablecido la conexión con la base de datos!','Ya puede usar el bot con normalidad.')
         .setFooter('Gracias por su paciencia.');
 
-    const client = new MongoClient(uri, { useNewUrlParser: true, reconnectTries: 10, reconnectInterval: 1000, autoReconnect: true });
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, autoReconnect: true });
 
+    // Establecemos conexión con la base de datos.
     client.connect(function(err)
     {
         if(err)
         {
             bot.mongostatus = false;
-            bot.channels.find('name','path-of-exile-bot').send(noConnectionMessage);
-            return;
+            return bot.channels.find(w => w.name === 'path-of-exile-bot').send(noConnectionMessage);
         }
         bot.db = client.db('items'); 
     });
@@ -185,14 +116,14 @@ function connectToDB()
     client.on('close', function ()
     {
         bot.mongostatus = false;
-        bot.channels.find('name','path-of-exile-bot').send(noConnectionMessage);
+        bot.channels.find(w => w.name === 'path-of-exile-bot').send(noConnectionMessage);
     });
 
     // Evento ejecutado cuando se recupera la conexión con la base de datos.
     client.on('reconnect', function ()
     {
         bot.mongostatus = true;
-        bot.channels.find('name','path-of-exile-bot').send(connectionMessage);
+        bot.channels.find(w => w.name === 'path-of-exile-bot').send(connectionMessage);
     });
 }
 
--- src/alerts.js
+++ src/alerts.js
@@ -1,8 +1,100 @@
 const request = require('request');
 const cron = require('node-cron');
 
+var bot;
+
 module.exports = {
-    foo: function () {
-    // whatever
+    setInitialAlerts: function(clientBot)
+    {
+        bot = clientBot;
+        if(!bot.mongostatus) return; // ! SET MSG
+
+        const alertsCollection = bot.db.collection('alerts');
+
+        // Por cada guild creamos un array con las alertas que esta tiene.
+        bot.guilds.forEach(guild =>
+        {
+            bot.alerts.set(guild.id, []);
+            bot.botCurrentChannel = guild.channels.find(channel => channel.name === 'path-of-exile-bot');
+
+            // Buscamos las alertas de la guild.
+            alertsCollection.find({ guildId: guild.id }).toArray((error, guildAlerts) =>
+            {
+                if(error) return console.log('error on find alerts'); // ! SET MSG
+
+                // Establecemos un cron sobre cada una de las alertas.
+                guildAlerts.forEach(alert =>
+                {
+                    this.setAlert(alert);
+                });
+
+            });
+        });
+    },
+    setAlert: function(alertElem)
+    {
+        const actualAlerts = bot.alerts.get(alertElem.guildId);
+
+        // Creamos un nuevo cron para una tarea determinada.
+        var alertCron = cron.schedule(`*/${alertElem.refresh} * * * *`, () =>{
+            showAlertToUser(alertElem);
+        }, { scheduled: false });
+
+        // Guardamos el cron en un Map identificado por el guild id.
+        alertCron.alert = alertElem.itemName;
+        actualAlerts.push(alertCron);
+        bot.alerts.set(alertElem.guildId, actualAlerts);
+
+        // Iniciamos la ejecución de los cron.
+        bot.alerts.get(alertElem.guildId).forEach(cronAlert => {
+            cronAlert.start();
+        });
+
+        /**
+         * Realiza la petición a la API para obtener los datos sobre el item del cual
+         * se estableció la alerta.
+         */
+        function showAlertToUser(alert)
+        {
+            if(alert.class == 'Currency' || alert.class == 'Fossils')
+            {
+                request({ url: `https://poe.ninja/api/data/currencyoverview?league=Legion&type=${alert.class.replace(/\s+/g,'')}`, json:true }, function (error, response, json)
+                {
+                    if(error) return console.log('error on request'); // ! SET MSG
+                    if(response.statusCode != 200) return console.log('not 200 status'); // ! SET MSG
+                    parseDataCurrencyOverview(json, alert);
+                });
+            }
+            else
+            {
+                request({ url: `https://poe.ninja/api/data/itemoverview?league=Legion&type=${alert.class.replace(/\s+/g,'')}`, json:true }, function (error, response, json)
+                {
+                    if(error) return console.log('error on request'); // ! SET MSG
+                    if(response.statusCode != 200) return console.log('not 200 status'); // ! SET MSG
+                    parseDataItemOverview(json, alert);
+                });
+            }
+        }
+
+        /**
+         * Se encarga de procesar el json de los items con clase Currency o Fossils.
+         */
+        function parseDataCurrencyOverview(data, alert)
+        {
+            const currencyDetails = data['currencyDetails'];
+            const lines = data['lines'];
+            const itemData = lines.find(item => item['detailsId'] == alert.itemName);
+            console.log(itemData);
+        }
+
+        /**
+         * Se encarga de procesar el json de los items que no correspondan con las clases Currency o Fossils.
+         */
+        function parseDataItemOverview(data, alert)
+        {
+            const lines = data['lines'];
+            const itemData = lines.find(item => item['detailsId'] == alert.itemName);
+            console.log(itemData);
+        }
     }
 };
